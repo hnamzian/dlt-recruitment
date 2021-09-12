84dba24cd6509d670720d01ce073fe5ef7807259
hnamzian <namazian87@gmail.com>
2021-09-12 11:28:03 +0300

    Add hardhat config containing compiler and network configs

---
+++ tests/simple-pos/hardhat.config.js
@@ -0,0 +1,31 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.5.16",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    localhost: {
+      live: false,
+      saveDeployments: true,
+      tags: ["local"],
+    },
+    hardhat: {
+      live: false,
+      saveDeployments: true,
+      initialBaseFeePerGas: 0,
+      tags: ["test", "local"],
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    artifacts: "./artifacts",
+  },
+};
